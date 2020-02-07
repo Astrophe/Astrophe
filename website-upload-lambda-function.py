24079e2492eb3837165b234df498b99c5049c17f
singingvike <git@astropheenterprises.com>
2020-02-07 18:35:04 -0500

    update website lambda function

--- website-upload-lambda-function.py
+++ website-upload-lambda-function.py
@@ -1,18 +1,23 @@
+import json
 import boto3
 import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
 
-website_bucket = s3.Bucket('website.astropheenterprises.com')
-websitebuild_bucket = s3.Bucket('websitebuild.astropheenterprises.com')
+    s3 = boto3.resource('s3')
 
-website_zip = io.BytesIO()
-websitebuild_bucket.download_fileobj('websitebuild.zip', website_zip)
+    website_bucket = s3.Bucket('website.astropheenterprises.com')
+    websitebuild_bucket = s3.Bucket('websitebuild.astropheenterprises.com')
 
-with zipfile.ZipFile(website_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        website_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        website_bucket.Object(nm).Acl().put(ACL='public-read')
+    website_zip = io.BytesIO()
+    websitebuild_bucket.download_fileobj('websitebuild.zip', website_zip)
+
+    with zipfile.ZipFile(website_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            website_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            website_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return 'Hello from Lambda'
