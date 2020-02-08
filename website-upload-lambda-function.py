46f7f7f2c1d90ab9b78d6f644929abab479cab82
singingvike <git@astropheenterprises.com>
2020-02-07 19:09:10 -0500

    updated deploy function to send SNS to confirm publish success or failure

--- website-upload-lambda-function.py
+++ website-upload-lambda-function.py
@@ -5,19 +5,28 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:988008946434:deployAEWebsiteTopic')
 
-    s3 = boto3.resource('s3')
+    try:
+        s3 = boto3.resource('s3')
+    
+        website_bucket = s3.Bucket('website.astropheenterprises.com')
+        websitebuild_bucket = s3.Bucket('websitebuild.astropheenterprises.com')
 
-    website_bucket = s3.Bucket('website.astropheenterprises.com')
-    websitebuild_bucket = s3.Bucket('websitebuild.astropheenterprises.com')
+        website_zip = io.BytesIO()
+        websitebuild_bucket.download_fileobj('websitebuild.zip', website_zip)
 
-    website_zip = io.BytesIO()
-    websitebuild_bucket.download_fileobj('websitebuild.zip', website_zip)
+        with zipfile.ZipFile(website_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                website_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                website_bucket.Object(nm).Acl().put(ACL='public-read')
 
-    with zipfile.ZipFile(website_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            website_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            website_bucket.Object(nm).Acl().put(ACL='public-read')
+        topic.publish(Subject="AEWebsite Published", Message="AE Website Updated")
+
+    except:
+        topic.publish(Subject="AEWebsite Publish Failed", Message="AE Site Deploy Failed")
+        raise
 
     return 'Hello from Lambda'
